f4fe9548ea896861a1d66909381ff41ca1486c33
frank mwangi <frank.mwangi@dadanadagroup.com>
2026-03-14 15:57:05 +0300

    chore: prj init

---
+++ src/main/java/com/codify/universaltracker/UniversalTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.codify.universaltracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversalTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversalTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codify/universaltracker/UniversalTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codify.universaltracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversalTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
